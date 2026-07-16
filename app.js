8effbb23b2b48d656db74542b63c242700088fa6
boquernoel-wq <boquernoel@gmail.com>
2026-07-16 10:41:48 +0800

    update

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ function calculateGradePayload() {
         letterGrade = "B";
     } else if (finalWeightedScore <= 99) {
         letterGrade = "A";
-    } else if (finalWeightedScore === 100) {
+    } else {
         letterGrade = "Perfect A+";
     }
 
